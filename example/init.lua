ad703503e369f9b54504753e755d934a97e6ee18
satoqz <40795431+satoqz@users.noreply.github.com>
2021-12-01 21:22:05 +0100

    by

--- example/init.lua
+++ example/init.lua
@@ -1,13 +1,13 @@
 hs.loadSpoon("spooner")
 
 spoon.spooner:start {
-  -- app names belinging to windows to fully ignore
+  -- app names belonging to windows to fully ignore
   ignore = { "System Preferences" },
   -- default amount of master windows
   masters = 1,
   -- default amount of padding around windows
   pad = 8,
-  -- amount to in- or decrease window padding when calling `incPad` or `decPad`
+  -- amount to in- or decrease window padding by when calling `incPad` or `decPad`
   padStep = 8,
   -- time in seconds for windows to finish transitioning to a new frame
   transition = 0.1,
